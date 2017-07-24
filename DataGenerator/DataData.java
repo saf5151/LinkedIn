4fef28b4e19a4f516bc35f47498dffa01c6c427b
EthanRB <30417843+EthanRB@users.noreply.github.com>
2017-07-24 15:26:22 -0400

    DataGenerator
    Not finished but these files generate sample data

---
+++ DataGenerator/DataData.java
@@ -0,0 +1,10 @@
+
+public class DataData {
+	public static int SKILLS = 50;
+	public static int JACKSON = 40;
+	public static int ROLE1 = 22;
+	public static int ROLE2 = 37;
+	public static int ROLE3 = 25;
+	public static int STATES = 50;
+	public static int CITIES = 32;
+}
