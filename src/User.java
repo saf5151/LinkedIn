d8e2cccccbf0d77948e92bb7c0f3185093430f1d
Aziz <axa8626@g.rit.edu>
2017-07-28 16:40:14 -0400

    Added help statements
    Added help statements for the userLoop

--- src/User.java
+++ src/User.java
@@ -25,8 +25,31 @@ public abstract class User
 	    		user_input = user_inputStr.split(" ");
 	    		//System.out.println(user_input[0]);
 	    		if ((user_input[0].toUpperCase()).equals("HELP")) {
-	    			System.out.println("-SearchEmployee: Search for other Employees.\n\tSyntax: SearchEmployee -name <Employee ID/null> -follow <true/false/null>\n");
-	    			System.out.println("-ViewEmployee: View another employees profile.\n\tSyntax: ViewEmployee -id <Employee ID>\n");
+	    			System.out.println("-SearchEmployee: Search for an Employee.\n\tSyntax: SearchEmployee -name <Employee Name/null> -follow <true/false/null>\n");
+	    			System.out.println("-ViewEmployee: View an employees profile.\n\tSyntax: viewEmployee -id <Employee ID>\n");
+	    			System.out.println("-searchCompany: search for a company.\n\tSyntax: searchCompany -name <Company Name/null> -location <City/State/City,State/null> -following <true/false/null>\n");
+	    			System.out.println("-viewCompany: view a company profile.\n\tSyntax: viewCompany -id <Company ID>\n");
+	    			System.out.println("-searchJob: search for a job listing.\n\tSyntax: searchJob -id <Company ID/null> -role <Job Role> -following <true/false/null>\n");
+	    			System.out.println("-editProfileAttribute: edit an attribute in the user profile.\n\tSyntax: editProfileAttribute -name <Attribute Name> -new <New Attribute Value>\n");
+	    			System.out.println("-addPhone: add a new phone number to a user profile.\n\tSyntax: addPhone -newNumber <Phone Number> -type <type>\n");
+	    			System.out.println("-addDelete: delete a phone number from a user profile.\n\tSyntax: deletePhone -number <Phone Number>\n");
+	    			System.out.println("-viewReview: view the company reviews.\n\tSyntax: viewReviews -id <Company ID>\n");
+	    			if (isEmp) {
+	    				System.out.println("-makeConnection: make a connection with another employee.\n\tSyntax: makeConnection -id <Company ID>\n");
+	    				System.out.println("-followCompany: follow a company to view their profile.\n\tSyntax: followCompany -id <Company ID>\n");
+	    				System.out.println("-reviewCompany: write a review about a company.\n\tSyntax: reviewCompany -id <Company ID> -description <review description> -recommended <true/false/null>\n");
+	    				System.out.println("-endorseSkill: endorse an employee's skill.\n\tSyntax: endorseSkill -id <Employee ID> -skillName <Skill Name>\n");
+	    				System.out.println("-addSkill: add a skill to user profile.\n\tSyntax: addSkill -name <Skill Name> -description <Description>\n");
+	    				System.out.println("-deleteSkill: delete a skill to user profile.\n\tSyntax: deleteSkill -name <Skill Name>\n");
+	    				System.out.println("-addPastJob: add a past job to profile.\n\tSyntax: addPastJob -role <Job Role> -id <Company id> -description <Job description>\n");
+	    				System.out.println("-deletePastJob: delete a past job from profile.\n\tSyntax: deletePastJob -role <Job Role> -id <Company id>\n");
+	    			}else {
+	    				System.out.println("-addAssociate: add another company as an associate.\n\tSyntax: addAssociate -id <Company ID>\n");
+	    				System.out.println("-postJob: post a new job listing.\n\tSyntax: postJob -role <Job Role> -salary <Job Salary/null> -description <Job description>\n");
+	    				System.out.println("-deleteJob: delete a job listing .\n\tSyntax: deleteJob -role <Job Role>\n");
+	    				System.out.println("-addOffice: add a company office location.\n\tSyntax: addOffice -location <CitySstate>\n");
+	    				System.out.println("-deleteOffice: delete an office location.\n\tSyntax: deleteOffice -location <City,State>\n");
+	    			}
 	    		//searchEmployee call
 	    		}else if((user_input[0].toUpperCase()).equals("SEARCHEMPLOYEE")) {
 	    			if ((user_input[4].toUpperCase()).equals("TRUE")) {
